94e6f51c877e5c099bf853f327b2375fbb0ea611
carlosmedina06 <slaawmedina14@gmail.com>
2023-05-26 03:57:37 -0300

    [add] principal logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,332 @@
-import Image from 'next/image'
+'use client'
+import { useRef, useEffect, useMemo } from 'react'
 
-export default function Home() {
+import { Position, Frames } from './types'
+import { collisions } from './data/collisions'
+
+const Home = () => {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null)
+  const image = useMemo(() => new Image(), [])
+  const playerImage = useMemo(() => new Image(), [])
+
+  image.src = '/images/Mapa.png'
+  playerImage.src = '/images/characterFront.png'
+
+  useEffect(() => {
+    const canvas = canvasRef.current
+    const ctx = canvas?.getContext('2d')
+
+    if (ctx && canvas) {
+      canvas.width = 2000
+      canvas.height = 1040
+
+      const collisionsMap = []
+
+      for (let i = 0; i < collisions.length; i += 120) {
+        collisionsMap.push(collisions.slice(i, 120 + i))
+      }
+
+      class Boundary {
+        static width = 48
+        static height = 48
+        position: Position
+        width: number
+        height: number
+        constructor({ position }: { position: Position }) {
+          this.position = position
+          this.width = 48
+          this.height = 48
+        }
+
+        draw?() {
+          if (!ctx) return
+          ctx.fillStyle = 'rgba(255, 0, 0, 0)'
+          ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+        }
+      }
+
+      const boundaries: Boundary[] = []
+
+      const offset = {
+        x: -1570,
+        y: -1920,
+      }
+
+      collisionsMap.forEach((row, rowIndex) => {
+        row.forEach((col, colIndex) => {
+          if (col === 15875)
+            boundaries.push(
+              new Boundary({
+                position: {
+                  x: colIndex * Boundary.width + offset.x,
+                  y: rowIndex * Boundary.height + offset.y,
+                },
+              }),
+            )
+        })
+      })
+
+      class Sprite {
+        position: Position
+        image: HTMLImageElement
+        frames: Frames
+        width: number
+        height: number
+        onImageLoad: (() => void) | undefined
+
+        constructor({
+          position,
+          image,
+          frames = { max: 1 },
+          width,
+          height,
+          onImageLoad,
+        }: {
+          position: Position
+          image: HTMLImageElement
+          frames?: Frames
+          width?: number
+          height?: number
+          onImageLoad?: () => void
+        }) {
+          this.position = position
+          this.image = image
+          this.frames = frames
+          this.width = width || 0
+          this.height = height || 0
+          this.onImageLoad = onImageLoad
+
+          this.image.onload = () => {
+            this.width = this.image.width / this.frames.max
+            this.height = this.image.height
+            if (this.onImageLoad) {
+              this.onImageLoad()
+            }
+          }
+        }
+
+        draw() {
+          if (canvas && ctx) {
+            ctx.drawImage(
+              this.image,
+              0,
+              0,
+              this.image.width / this.frames.max,
+              this.image.height,
+              this.position.x,
+              this.position.y,
+              this.image.width / this.frames.max,
+              this.image.height,
+            )
+          }
+        }
+      }
+
+      const player = new Sprite({
+        position: {
+          x: canvas.width / 2 - 143 / 4 / 2,
+          y: canvas.height / 2 - 55 / 2,
+        },
+        image: playerImage,
+        frames: {
+          max: 4,
+        },
+      })
+
+      const background = new Sprite({
+        position: {
+          x: offset.x,
+          y: offset.y,
+        },
+        image,
+      })
+
+      const keys = {
+        w: {
+          pressed: false,
+        },
+        a: {
+          pressed: false,
+        },
+        s: {
+          pressed: false,
+        },
+        d: {
+          pressed: false,
+        },
+      }
+
+      const movables = [background, ...boundaries]
+
+      const objectCollides = ({ obj1, obj2 }: { obj1: Sprite; obj2: Boundary }) => {
+        return (
+          obj1.position.x + obj1.width >= obj2.position.x &&
+          obj1.position.x <= obj2.position.x + obj2.width &&
+          obj1.position.y <= obj2.position.y + obj2.height &&
+          obj1.position.y + obj1.height >= obj2.position.y
+        )
+      }
+
+      const animate = () => {
+        window.requestAnimationFrame(animate)
+        background.draw()
+        boundaries.forEach((boundary) => {
+          if (boundary.draw) boundary.draw()
+        })
+
+        player.draw()
+        let moving = true
+
+        if (keys.w.pressed && lastKey === 'w') {
+          for (let i = 0; i < boundaries.length; i++) {
+            const boundary = boundaries[i]
+
+            if (
+              objectCollides({
+                obj1: player,
+                obj2: {
+                  ...boundary,
+                  position: {
+                    x: boundary.position.x,
+                    y: boundary.position.y + 3,
+                  },
+                },
+              })
+            ) {
+              moving = false
+              break
+            }
+          }
+          if (moving)
+            movables.forEach((movable) => {
+              movable.position.y += 3
+            })
+        }
+        if (keys.a.pressed && lastKey === 'a') {
+          for (let i = 0; i < boundaries.length; i++) {
+            const boundary = boundaries[i]
+
+            if (
+              objectCollides({
+                obj1: player,
+                obj2: {
+                  ...boundary,
+                  position: {
+                    x: boundary.position.x + 3,
+                    y: boundary.position.y,
+                  },
+                },
+              })
+            ) {
+              moving = false
+              break
+            }
+          }
+          if (moving)
+            movables.forEach((movable) => {
+              movable.position.x += 3
+            })
+        }
+        if (keys.s.pressed && lastKey === 's') {
+          for (let i = 0; i < boundaries.length; i++) {
+            const boundary = boundaries[i]
+
+            if (
+              objectCollides({
+                obj1: player,
+                obj2: {
+                  ...boundary,
+                  position: {
+                    x: boundary.position.x,
+                    y: boundary.position.y - 3,
+                  },
+                },
+              })
+            ) {
+              moving = false
+              break
+            }
+          }
+          if (moving)
+            movables.forEach((movable) => {
+              movable.position.y -= 3
+            })
+        }
+        if (keys.d.pressed && lastKey === 'd') {
+          for (let i = 0; i < boundaries.length; i++) {
+            const boundary = boundaries[i]
+
+            if (
+              objectCollides({
+                obj1: player,
+                obj2: {
+                  ...boundary,
+                  position: {
+                    x: boundary.position.x - 3,
+                    y: boundary.position.y,
+                  },
+                },
+              })
+            ) {
+              moving = false
+              break
+            }
+          }
+          if (moving)
+            movables.forEach((movable) => {
+              movable.position.x -= 3
+            })
+        }
+      }
+
+      animate()
+
+      let lastKey = ''
+
+      window.addEventListener('keydown', ({ key }) => {
+        switch (key) {
+          case 'w':
+            keys.w.pressed = true
+            lastKey = 'w'
+            break
+          case 'a':
+            keys.a.pressed = true
+            lastKey = 'a'
+            break
+          case 's':
+            keys.s.pressed = true
+            lastKey = 's'
+            break
+          case 'd':
+            keys.d.pressed = true
+            lastKey = 'd'
+        }
+      })
+
+      window.addEventListener('keyup', ({ key }) => {
+        switch (key) {
+          case 'w':
+            keys.w.pressed = false
+            break
+          case 'a':
+            keys.a.pressed = false
+            break
+          case 's':
+            keys.s.pressed = false
+            break
+          case 'd':
+            keys.d.pressed = false
+            break
+        }
+      })
+    }
+  }, [image, playerImage])
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div>
+      <canvas ref={canvasRef} height={400} width={400} />
+    </div>
   )
 }
+
+export default Home
