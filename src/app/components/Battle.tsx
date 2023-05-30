3c8722cd10622981aae36480b5279c88dea96259
carlosmedina06 <slaawmedina14@gmail.com>
2023-05-30 01:18:52 -0300

    [add] battle scene component

--- src/app/components/Battle.tsx
+++ src/app/components/Battle.tsx
@@ -1,13 +1,21 @@
-import React from 'react'
+'use client'
 
-interface BattleProps {
-  children?: React.ReactNode
-}
+import Image from 'next/image'
 
-export const Battle: React.FC<BattleProps> = ({ children }) => {
+import PokemonEncounter from './PokemonEncounter'
+
+export const Battle = () => {
   return (
-    <div>
-      <div className="bg-black min-w-full h-screen">{children}</div>
+    <div className="relative">
+      <div className="absolute top-10 w-11/12 mx-6">
+        <PokemonEncounter />
+      </div>
+      <Image
+        alt="battlebackground"
+        height={window.innerHeight}
+        src={'/images/battleBackground.png'}
+        width={window.innerWidth}
+      />
     </div>
   )
 }
